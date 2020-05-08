f7d6600a91eb8c24770165c4590cbf5198d33091
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2020-05-08 19:30:34 +0200

    modifications serveur (transmission_fichiers)

--- Sprint3/Version1/serveur.c
+++ Sprint3/Version1/serveur.c
@@ -9,12 +9,13 @@
 #define TMAX 65000 //taille maximum des paquets (en octets)
 #define nbrClientMax 200
 
-int tabdSCFile[200]; // tableau de 200 sockets pour les fichiers
 int tabdSC[200] ; //tableau de 200 sockets
+int tabdSCFichier[200]; //200 sockets pour les fichiers
 char pseudos[200][10];
 int nbrClient;
 int fin = 0;
-pthread_t thread[nbrClientMax];
+pthread_t thread[nbrClientMax]; //pour les messages
+pthread_t thread2[nbrClientMax]; //pour les fichiers
 
 //fonction pour envoyer un message
 int envoie(int SockE, char* message) {
@@ -94,7 +95,7 @@ void recuperer_pseudo (char *pseudo, int i){
 void * transmission (void * args){
 
     int *recupVoid = args;
-    int i = *recupVoid;
+    int i = *recupVoid; //Expéditeur message
     char msg[TMAX] = "";
 
     while(fin != 1){
@@ -106,6 +107,8 @@ void * transmission (void * args){
         if(strcmp(msg,"fin\n") == 0) {
 			fin = 1;
 		}
+
+       
 
 		printf("le serveur a reçu %s", msg);
 
@@ -122,18 +125,110 @@ void * transmission (void * args){
                 if (envoie(tabdSC[j], newMsg) != 1){
 	                perror("err : env dans trans");
 	                pthread_exit(NULL);
-	                printf("message envoyé à client %d \n", j);
+	                
                 }
+                printf("message envoyé à client %d \n", j);
             }
             j = j+1;
         }
 
         printf("le serveur a envoyé %s", newMsg);
+
     }
     printf("je sors de la boucle transmission serveur \n");
     pthread_exit(NULL);
 }
 
+void* transmissionFichier(void* arg) {
+    int res;
+ 
+    int *recupVoid = arg;
+    int i = *recupVoid; //Expéditeur fichier
+    char msg[TMAX] = "";
+    
+
+
+    while (1) {
+
+        //recevoir le titre du fichier
+        if (reception(tabdSCFichier[i], msg) != 1){
+            perror("err : recep dans trans");
+            pthread_exit(NULL);
+        }
+        
+        int j = 0; 
+        while (j < nbrClient){
+            if (j != i) {
+                //on envoie à tout le nom du fichier
+                if (envoie(tabdSCFichier[j], msg) != 1){
+	                perror("erreur nom du fichier");
+	                pthread_exit(NULL);
+                }
+                printf("message envoyé à client %d \n", j);
+            }
+            j++; 
+        }
+        printf("envoi nom fichier en cours\n");
+
+
+        //recevoir la taille fichier
+        if (reception(tabdSCFichier[i], msg) != 1){
+            perror("err : recep dans trans");
+            pthread_exit(NULL);
+        }
+        int tailleFichier = atoi(msg);
+     
+       
+        j = 0; 
+        while (j < nbrClient){
+            if (j != i) {
+                //on envoie à tout le monde la taille du fichier
+                 if (envoie(tabdSCFichier[j], msg) != 1){
+	                perror("erreur envoie taille fichier");
+	                pthread_exit(NULL);
+                }
+                 printf("message envoyé à client %d \n", j);
+            }
+            j++; 
+        }
+        printf("envoi taille en cours\n");
+      
+        j=0;
+        int tailleEnvoye = 0;
+          //recevoir fichier
+            if (reception(tabdSCFichier[i], msg) != 1){
+            perror("err : recep dans trans");
+            pthread_exit(NULL);
+        }
+        while(tailleEnvoye < tailleFichier){
+        
+            while (j < nbrClient){
+                if (j != i) {
+
+                    
+                    //Envoi du fichier à tout le monde
+                    if (envoie(tabdSCFichier[j], msg) != 1){
+	                perror("erreur envoie  fichier");
+	                pthread_exit(NULL);    
+                    }
+                  printf("fichier envoyé à client %d \n", j);
+
+                    
+                }
+                
+                j++;
+            }
+            
+            j = 0;
+            memset(msg,0,sizeof(msg));
+            tailleEnvoye +=res;
+        }
+        
+    }
+    pthread_exit(0);
+
+    
+}
 
 //fonction de connexion
 void * connexion (void * args){
@@ -149,8 +244,6 @@ void * connexion (void * args){
 
     while (nbrClient < nbrClientMax && fin == 0) {
 
-            printf("je suis dans la fonction connexion \n");
-
             tabdSC[i] = accept(dS, (struct sockaddr*) &aC,&lg);
             if(tabdSC[i] == -1){
                 perror("Erreur accept client \n");
@@ -160,24 +253,19 @@ void * connexion (void * args){
             printf ("j'ai accepté un nouveau client \n");
 
             recuperer_pseudo (pseudos[i], i);
-
-            printf ("j'ai récupéré le pseudo %s", pseudos[i]);
-
-            tabdSCFile[i] = accept(dS, (struct sockaddr*) &aC,&lg);
-            if(tabdSCFile[i] == -1){
-                perror("Erreur accept client \n");
-                pthread_exit(NULL);
-            }
-
             printf("client numéro %d connecté avec le pseudo %s \n", i+1, pseudos[i]);
 
             j = i;
             i = i + 1;
 
             if (pthread_create(&thread[j], NULL, transmission, &j ) != 0){
-                perror("creation de thread[i]");
+                perror("erreur création thread pour message");
                 pthread_exit(NULL);
-            } else {
+            }
+            if (pthread_create(&thread2[j],NULL, transmissionFichier, &j) != 0){
+                perror("erreur création thread pour fichier");
+                pthread_exit(NULL);
+            }else {
                 nbrClient = nbrClient + 1;
                 printf("nombre client = %d \n", nbrClient);
             }
